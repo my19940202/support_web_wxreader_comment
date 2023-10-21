831acd1596abb85b8ce8920c4328cbdd7e263831
xishengbo <xishengbo@baidu.com>
2023-10-21 19:40:59 +0800

    使用create-react-app 搭建本地react项目

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# support_web_wxreader_comment
+# Create React App 自动化生成的 ts-react 项目
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <div className="body">
+                this is app.tsx created by create-react-app
+            </div>
+        </div>
+    );
+}
+
+export default App;
